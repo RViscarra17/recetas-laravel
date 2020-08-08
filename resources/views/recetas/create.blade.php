f59a4548d6f8bbfc4c8aacc339d73493329c0740
VM17015 <VM17015@ues.edu.sv>
2020-08-07 18:37:30 -0600

    App, Index y Create de Recetas

---
+++ resources/views/recetas/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('botones')
+    <a class="btn btn-primary mr-2" href="{{ route('recetas.index') }}">Volver</a>
+@endsection
+
+@section('content')
+
+    <h2 class="text-center mb-5">Crear receta</h2>
+    
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            
+        </div>
+    </div>
+
+@endsection
