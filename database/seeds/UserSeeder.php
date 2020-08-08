e6e7acbe53851a97c77732bbd9bbc9abe359de28
VM17015 <VM17015@ues.edu.sv>
2020-08-07 21:36:19 -0600

    Migration recetas, seeder de categoria_recetas y user

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Rafa',
+            'email' => 'rafa@hotmail.com',
+            'password' => Hash::make('12345678'),
+            'url' => 'http://recetas.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Juan',
+            'email' => 'correo@hotmail.com',
+            'password' => Hash::make('12345678'),
+            'url' => 'http://recetas.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
